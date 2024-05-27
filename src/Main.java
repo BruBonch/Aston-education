ccfd60e65c7c7eccc936d780573c89cf0e8fe9d0
Bru Bonch <Wexller35@gmail.com>
2024-05-27 13:55:02 +0300

    Creates employees array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee[] employeesArray = {
+                new Employee(
+                        "Petrov Ivan",
+                        "junior QA engineer",
+                        "PetrovI@mail.ru",
+                        "8921 135 56 78",
+                        "45000 руб.",
+                        25
+                ),
+                new Employee(
+                        "Antonov Igor",
+                        "junior AQA engineer",
+                        "AntI@yandex.ru",
+                        "8911 134 56 78",
+                        "55000 руб.",
+                        28
+                ),
+                new Employee(
+                        "Zayceva Ksenia",
+                        "middle QA engineer",
+                        "Zaycevaa@gmail.com",
+                        "8952 111 22 33",
+                        "65000 руб.",
+                        23
+                ),
+                new Employee(
+                        "Averin Artem",
+                        "middle Java developer",
+                        "AArtem@mail.ru",
+                        "8911 189 43 52",
+                        "80000 руб.",
+                        29
+                ),
+                new Employee(
+                        "Fomina Anna",
+                        "junior Java developer",
+                        "FAnna@yandex.ru",
+                        "8911 999 35 85",
+                        "70000 руб.",
+                        29
+                )
+        };
     }
 }
