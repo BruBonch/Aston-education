77eac02c8a17976d355d3225a19e543192c91e0b
Bru Bonch <Wexller35@gmail.com>
2024-06-23 15:16:53 +0300

    Calls the method to get the factorial in the main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        FactorialCalculator factorialCalculator = new FactorialCalculator();
+        System.out.println(factorialCalculator.getFactorial(10));
     }
 }
