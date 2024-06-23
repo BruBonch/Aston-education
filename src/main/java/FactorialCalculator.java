4ee732ee4c6af09bfadb61bfb68583c1b01d8f0a
Bru Bonch <Wexller35@gmail.com>
2024-06-23 15:09:29 +0300

    Adds test methods to the test class

--- src/main/java/FactorialCalculator.java
+++ src/main/java/FactorialCalculator.java
@@ -1,2 +1,15 @@
+import exceptions.NegativeNumberException;
+
 public class FactorialCalculator {
+    public  int getFactorial(int f) {
+        if (f < 0) {
+            throw new NegativeNumberException("Число для рассчета факториала не может быть отрицательным");
+        }
+
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
 }
--- src/test/java/FactorialCalculatorTest.java
+++ src/test/java/FactorialCalculatorTest.java
@@ -1,2 +1,37 @@
+import exceptions.NegativeNumberException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 public class FactorialCalculatorTest {
+    FactorialCalculator factorialCalculator;
+
+    @DataProvider
+    public static Object[] getNumbers() {
+        return new Object[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+
+    @BeforeClass
+    public void setUp() {
+        factorialCalculator = new FactorialCalculator();
+    }
+
+    @Test
+    public void factorialFromZero() {
+        int expected = 1;
+        int actual = factorialCalculator.getFactorial(0);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(expectedExceptions = NegativeNumberException.class,
+            expectedExceptionsMessageRegExp = "Число для рассчета факториала не может быть отрицательным")
+    public void factorialFromNegativeNumber() {
+        factorialCalculator.getFactorial(-3);
+    }
+
+    @Test(dataProvider = "getNumbers")
+    public void factorialFromPositiveNumber(Integer number) {
+
+    }
 }
